31324abecdb781b4b855fb4fa2ee16e30ff50abe
speexy <jennifer.meyer@posteo.de>
2018-01-05 19:22:38 +0100

    check if number of decimal places is the problem of svg-clip in ff. it isn't. looks nicer anyway though, so let's keep it.

--- src/components/svg/ClipPositionen.js
+++ src/components/svg/ClipPositionen.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ClipPositionen = () => (
   <svg
-    viewBox="0 0 1440 520"
+    viewBox="0 0 1 1"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     preserveAspectRatio="none">
-    <clipPath id="clip_positionen" clipPathUnits="objectBoundingBox" transform="scale(0.000694444 0.001923077)">
-      <polygon id="path-1" points="0 0 1440 0 1440 520 319.859375 490 0 520"></polygon>
+    <clipPath id="clip_positionen" clipPathUnits="objectBoundingBox">
+      <polygon id="path-1" points="0,0 1,0 1,1 0.222,0.942 0,1"></polygon>
     </clipPath>
   </svg>
 );
