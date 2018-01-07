0f8559b1cad519fd3112c335f45b6d42ba395c6e
speexy <jennifer.meyer@posteo.de>
2018-01-07 17:32:46 +0100

    opacity for ie for lower triangle

--- src/components/svg/TriangleBottom.js
+++ src/components/svg/TriangleBottom.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './svg.module.css';
 
 const TriangleBottom = () => (
-  <svg width="87px" height="78px" viewBox="0 0 87 78" version="1.1" xmlns="http://www.w3.org/2000/svg" >
+  <svg width="87px" height="78px" viewBox="0 0 87 78" version="1.1" xmlns="http://www.w3.org/2000/svg" className={styles.triangle_bottom}>
     <g id="Screens" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g id="Netzwerk" transform="translate(-523.000000, -1802.000000)" fill="#05FFA9">
         <polygon id="Triangle-3-Copy-6" style={{ mixBlendMode: 'multiply' }} transform="translate(566.500000, 1841.000000) rotate(180.000000) translate(-566.500000, -1841.000000) " points="523 1880 610 1802 523 1802"></polygon>
