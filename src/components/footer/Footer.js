debacc8c546baabc7fee57c02e1d4d81059f29cc
speexy <jennifer.meyer@posteo.de>
2018-01-08 11:16:06 +0100

    footer with logo, links and fb-icon as placeholder

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,42 +1,31 @@
 import React from 'react';
 import BackgroundFooter from '../svg/BackgroundFooter.js';
-import SendLogoNegative from '../svg/SendLogoNegative.js';
+import logo from '../../img/logo-negative.svg';
 import Facebook from '../svg/Facebook.js';
-import FacebookHover from '../svg/FacebookHover.js';
 import styles from './footer.module.css';
 import Link from 'gatsby-link';
 
 
 const Footer = () => (
-	<div>
-		<BackgroundFooter />
-		<footer className={styles.footer}>
-			<div className={styles.top}>				
-				<div className={styles.links}>
-					<SendLogoNegative className={styles.send_logo_negative} />
-					<ul className={styles.socialNetworks}>
-						<li><Link to="/positionen"><Facebook /></Link></li>	
-						<li><Link to="/positionen"><Facebook /></Link></li>					
-						<li><Link to="/positionen"><Facebook /></Link></li>																			
-					</ul>
-					<ul className={styles.linkList}>
-						<li><Link to="/positionen">Presse</Link></li>	
-						<li><Link to="/positionen">Kontakt</Link></li>		
-						<li><Link to="/impressum">Impressum</Link></li>																															
-					</ul>
-					<ul className={styles.linkList}>
-						<li><Link to="/positionen">Über Uns</Link></li>	
-						<li><Link to="/positionen">Social Entrepreneurship</Link></li>
-						<li><Link to="/positionen">Positionen</Link></li>	
-						<li><Link to="/positionen">Netzwerk</Link></li>	
-					</ul>
-				</div>
-			</div>
-			<div className={styles.bottom}>
-			</div>
-		</footer>
-	</div>
+  <footer className={styles.footer}>
+    <BackgroundFooter />
+    <div>
+      <figure className={styles.figure_desktop}>
+        <img src={logo} alt="Send" />
+      </figure>
+      <ul className={styles.socialNetworks}>
+        <li><Link to="/positionen"><Facebook /></Link></li>
+        <li><Link to="/positionen"><Facebook /></Link></li>
+        <li><Link to="/positionen"><Facebook /></Link></li>
+      </ul>
+      <ul className={styles.linkList}>
+        <li><Link to="/positionen">Social Entrepreneurship</Link></li>
+        <li><Link to="/positionen">Netzwerk</Link></li>
+        <li><Link to="/impressum">Impressum</Link></li>
+      </ul>
+    </div>
+
+  </footer>
 );
 
 export default Footer;
-
