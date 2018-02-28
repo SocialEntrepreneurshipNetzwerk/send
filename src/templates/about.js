17edcd94fff47385748a3192be228f00ab03f255
speexy <jennifer.meyer@posteo.de>
2018-02-28 18:06:24 +0100

    turquoise section

--- src/templates/about.js
+++ src/templates/about.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TopImage from '../components/top-image/TopImage.js';
 import PageHelmet from '../components/PageHelmet';
 import topImage from '../img/netzwerk_min.jpg';
+import BackgroundTurquoise2 from '../components/svg/BackgroundTurquoise2';
 import styles from './about.module.css';
 
 
@@ -13,6 +14,10 @@ export default ({ data }) => {
     <div>
       <PageHelmet frontmatter={frontmatter}/>
       <TopImage imageSource={topImage}/>
+      <div className={styles.turquoise_section}>
+        <BackgroundTurquoise2/>
+        <p className={styles.tagline}>{frontmatter.tagline}</p>
+      </div>
     </div>
   );
 };
@@ -26,6 +31,7 @@ export const AboutPageQuery = graphql`
     markdownRemark(fields: { slug: { eq: "/ueber-uns" } }) {
         frontmatter {
             title
+            tagline
         }
     }
 }
