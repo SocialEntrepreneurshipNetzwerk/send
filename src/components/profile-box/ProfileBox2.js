e636f2c31ae95f1216726e0f96091b9d0beb704b
speexy <jennifer.meyer@posteo.de>
2018-03-01 17:52:37 +0100

    add different clip-path, mobile breakpoints for columns

--- src/components/profile-box/ProfileBox2.js
+++ src/components/profile-box/ProfileBox2.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ClipProfile from '../svg/ClipProfile';
+import ClipProfile2 from '../svg/ClipProfile2';
 import styles from './profile-box.module.css';
 
 const ProfileBox2 = ( props ) => (
-  <div className={styles.box2}>
-    <p className={styles.clip_profile} style={{ backgroundImage: `url(${props.content.image})` }}>
-      <ClipProfile/>
-    </p>
-    <p>{props.content.name}</p>
-    <p>{props.content.role}</p>
-    <p>{props.content.organization}</p>
+  <div className={styles.box2_wrapper}>
+    <div className={styles.box2}>
+      <p className={styles.clip_profile2} style={{ backgroundImage: `url(${props.content.image})` }}>
+        <ClipProfile2/>
+      </p>
+      <p>{props.content.name}</p>
+      <p>{props.content.role}</p>
+      <p>{props.content.organization}</p>
+    </div>
   </div>
 );
 
