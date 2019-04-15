9bfd50b9a8c09a5e242cb281dd07a85e93742215
speexy <jennifer.meyer@posteo.de>
2019-04-15 13:06:26 +0200

    change text and color of sticky cta

--- src/components/cta/StickyCTAConference.js
+++ src/components/cta/StickyCTAConference.js
@@ -3,8 +3,10 @@ import styles from './cta.module.css';
 
 const StickyCTA = () => (
   <a href="/konferenz">
-    <svg viewBox="0 0 292.07 104" version="1.1" xmlns="http://www.w3.org/2000/svg" className={styles.sticky_cta_conf}>
-      <path class="cls-1" fill="#05ffa9" d="M22,6.52,292.07,0V80.4L87.84,104,0,86.2Z"/><text class="cls-2" transform="translate(41.43 48.41)">SEND KONFERENZ<tspan x="0" y="24.11">SEI DABEI!</tspan></text>
+    <svg viewBox="0 0 452.07 124" version="1.1" xmlns="http://www.w3.org/2000/svg" className={styles.sticky_cta_conf}>
+      <path class="cls-1" fill="#ffff00" d="M22,6.52,452.07,0V80.4L87.84,124,0,86.2Z"/><text class="cls-2" transform="translate(41.43 48.41)">KONFERENZ RAUS AUS DER NISCHE
+
+<tspan x="0" y="36.11">JETZT ANMELDEN</tspan></text>
     </svg>
   </a>
 );
