77273b4969b142cbbd74c1ac3a75015fc29f0e0f
speexy <jennifer.meyer@posteo.de>
2019-04-15 19:26:21 +0200

    update page helmet to get og:image for facebook

--- src/components/PageHelmet.js
+++ src/components/PageHelmet.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-export default ({ frontmatter }) => (
-  <Helmet
-    title={`${frontmatter.title} | SEND`}
-    meta_title={frontmatter.title}
-  />
-);
+
+
+
+
+
+export default ({ frontmatter }) => {
+
+  const siteUrl = 'https://send-ev.de';
+  const siteLang = 'de_DE';
+  const siteName = 'Social Entrepreneur Netzwerk Deutschland';
+  const siteDescription = 'Das Netzwerk für Social Entrepreneure und Social Startups in Deutschland.';
+  const siteImage = `${siteUrl}/static/logo.svg`;
+
+
+  const pageTitle = frontmatter.title || siteName;
+  const pageDescription = frontmatter.excerpt || siteDescription;
+  const pageImage = frontmatter.imgage ? `${siteUrl}${frontmatter.imgage}` : siteImage;
+
+  return (
+    <Helmet
+      title={`${frontmatter.title} | SEND`}
+      meta={[
+        { name: 'title', content: `${pageTitle}` },
+        { name: 'description', content: `${pageDescription}` },
+        { property: 'og:locale', content: `${siteLang}` },
+        { property: 'og:site_name', content: `${siteName}` },
+        { property: 'og:title', content: `${pageTitle}` },
+        { property: 'og:description', content: `${pageDescription}` },
+        { property: 'og:image', content: `${pageImage}` },
+        { name: 'twitter:card', content: `${siteLang}` },
+        { name: 'twitter:title', content: `${pageTitle}` },
+        { name: 'twitter:description', content: `${pageDescription}` },
+        { name: 'twitter:image', content: `${pageImage}` }
+      ]}
+    />
+  )
+};
