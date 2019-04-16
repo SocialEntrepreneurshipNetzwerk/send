502428d0f5a6a34abcea9400f210886f78ad2274
Jonathan Moore <jon@jonfm.co.uk>
2019-04-16 14:25:05 +0200

    Ne content container to handle scripts

---
+++ src/components/ContentContainer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default class ContentContainer extends React.Component {
+  constructor() {
+    super()
+    this.content = null;
+  }
+
+
+  componentDidMount() {
+    const template = document.createElement( 'template' )
+    template.innerHTML = this.props.content;
+
+    const scripts = template.content.querySelectorAll( "script" );
+
+    scripts.forEach(( script ) => {
+      const s = document.createElement( 'script' );
+      s.type = 'text/javascript';
+      s.async = true;
+      s.innerHTML = script.innerHTML
+      s.src = script.src
+      s.id = script.id
+
+      script.replaceWith(s)
+    })
+    this.content.appendChild(template.content);
+  }
+
+  render() {
+    return (
+      <div ref={el => (this.content = el)}/>
+    )
+  }
+}
