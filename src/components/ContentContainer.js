436df81b4e48152cbcb86215b4801f7d4d76fcc5
speexy <jennifer.meyer@posteo.de>
2019-04-24 18:02:29 +0200

    add content for robots and remove after componentDidMount

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -1,34 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 export default class ContentContainer extends React.Component {
-  constructor(props) {
-    super()
+  constructor( props ) {
+    super();
     this.content = null;
   }
 
 
   componentDidMount() {
-    const template = document.createElement( 'template' )
+    const template = document.createElement( 'template' );
     template.innerHTML = this.props.content;
 
-    const scripts = template.content.querySelectorAll( "script" );
+    const scripts = template.content.querySelectorAll( 'script' );
 
     scripts.forEach(( script ) => {
       const s = document.createElement( 'script' );
       s.type = 'text/javascript';
       s.async = true;
-      s.innerHTML = script.innerHTML
-      s.src = script.src
-      s.id = script.id
+      s.innerHTML = script.innerHTML;
+      s.src = script.src;
+      s.id = script.id;
 
-      script.replaceWith(s)
-    })
-    this.content.appendChild(template.content);
+      script.replaceWith( s );
+    });
+    const remove = document.getElementById( 'robotContent' );
+    document.getElementById( 'contentParent' ).removeChild( remove );
+    this.content.appendChild( template.content );
   }
 
   render() {
     return (
-      <div ref={el => (this.content = el)} dangerouslySetInnerHTML={{ __html: this.props.content }}/>
-    )
+      <div id="contentParent">
+        <div id="robotContent" dangerouslySetInnerHTML={{ __html: this.props.content }} />
+        <div ref={el => ( this.content = el )} />
+      </div>
+    );
   }
 }
