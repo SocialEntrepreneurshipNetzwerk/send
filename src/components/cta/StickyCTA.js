95c68b4bf4433f28a8742015340f5575a0320cfd
speexy <jennifer.meyer@posteo.de>
2019-09-10 13:48:01 +0200

    require # when adding hex-color

--- src/components/cta/StickyCTA.js
+++ src/components/cta/StickyCTA.js
@@ -8,7 +8,7 @@ const StickyCTA = (props) => {
     <svg viewBox="0 0 267 104" version="1.1" xmlns="http://www.w3.org/2000/svg" className={styles.sticky_cta}>
       <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g id="Mitglied-werden--F-L-O-A-T-I-N-G">
-          <path className={styles.sticky_cta_polygon} fill={"#"+props.data.hexColor} d="m19.8 6.52 246-6.52v80.4l-186 23.6-80-17.8z"/>
+          <path className={styles.sticky_cta_polygon} fill={props.data.hexColor} d="m19.8 6.52 246-6.52v80.4l-186 23.6-80-17.8z"/>
         </g>
       </g>
       <switch>
--- src/pages/index.md
+++ src/pages/index.md
@@ -4,10 +4,10 @@ cta_sticky:
   text: Jetzt Mitglied werden!
   link: https://docs.google.com/forms/d/1SK6RcT-AMB_sZgGIkqQY8EOhIz_bnMuVSuJ7zCmd4Mg/viewform?edit_requested=true
   showOnPage: true
-  hexColor: ff007b
+  hexColor: '#ff007b'
 tagline: gemeinsam. wirken.
 tagline_large: |-
-  **Social Entrepreneurship Netzwerk Deutschland e.V.** 
+  **Social Entrepreneurship Netzwerk Deutschland e.V.**
 
   Das Netzwerk für Social Entrepreneure und Social Startups in Deutschland.
 paragraph: >-
